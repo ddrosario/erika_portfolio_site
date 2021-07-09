2402104940d315e9d612f95668baece9be00ba0b
ddrosario <drewdelrosario@gmail.com>
2021-07-08 19:39:30 -0700

    ✨🚧 Create base component for example projects

---
+++ components/EgProject.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import styles from '../styles/EgProject.module.scss';
+
+interface Project {
+  projectImg: string;
+  projectAlt: string;
+  title: string;
+  description: string;
+}
+
+const EgProject = ({
+  projectImg,
+  projectAlt,
+  title,
+  description,
+}: Project) => (
+  <article className={styles.egProject}>
+    <div className={styles.borderedProject}>
+      <div className={styles.project}>
+        <img
+          src={projectImg}
+          alt={projectAlt}
+        />
+      </div>
+    </div>
+    <div className={styles.spacer} />
+    <div className={styles.borderedProject}>
+      <div className={styles.project}>
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  </article>
+);
+
+export default EgProject;
