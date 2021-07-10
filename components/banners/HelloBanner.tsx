87dbcd4729c2ad6ce7e1365c2ecb927be2f23515
ddrosario <drewdelrosario@gmail.com>
2021-07-10 16:59:50 -0700

    💄 HelloBanner style update uses dynamic BannerBorder

--- components/banners/HelloBanner.tsx
+++ components/banners/HelloBanner.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+
+import BannerBorder from './BannerBorder';
+
 import styles from '../../styles/banners/HelloBanner.module.scss';
 
 const HelloBanner = () => (
-  <div className={styles.banner}>
-    <h1 className={styles.bannerHeader}>
-      <span className={styles.helloText}>Hello,</span>
-      <span className={styles.myName}>my name is Erika!</span>
-    </h1>
-    <h3 className={styles.bannerSubtext}>I&apos;m a Product Designer based in the Bay Area.</h3>
-  </div>
+  <BannerBorder>
+    <div className={styles.bannerPadding}>
+      <h1 className={styles.bannerHeader}>
+        <span className={styles.helloText}>{'Hello, '}</span>
+        <span className={styles.myName}>my name is Erika!</span>
+      </h1>
+      <h3 className={styles.bannerSubtext}>I&apos;m a Product Designer based in the Bay Area.</h3>
+    </div>
+  </BannerBorder>
 );
 
 export default HelloBanner;
