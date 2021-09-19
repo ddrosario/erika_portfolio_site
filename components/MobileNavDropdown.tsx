a677ab78de731ffb5c6f3bcf3f77758384f9cc4e
ddrosario <drewdelrosario@gmail.com>
2021-09-19 10:34:19 -0700

    🚧 Mobile navigation dropdown skeleton

---
+++ components/MobileNavDropdown.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import Link from 'next/link';
+
+import styles from '@styles/header/MobileNavDropdown.module.scss';
+
+const MobileNavDropdown = () => (
+  <ul className={styles.dropdown}>
+    <li>
+      <Link
+        href="/work"
+        passHref
+      >
+        <a
+          className={styles.link}
+          href="replace"
+        >
+          Work
+        </a>
+      </Link>
+    </li>
+    <li>
+      <Link
+        href="/about"
+        passHref
+      >
+        <a
+          className={styles.link}
+          href="replace"
+        >
+          About
+        </a>
+      </Link>
+    </li>
+    <li>
+      <Link
+        href="/Resume"
+        passHref
+      >
+        <a
+          className={styles.link}
+          href="replace"
+        >
+          Resume
+        </a>
+      </Link>
+    </li>
+    <li>
+      <Link
+        href="/contact-me"
+        passHref
+      >
+        <a
+          className={styles.link}
+          href="replace"
+        >
+          Contact Me
+        </a>
+      </Link>
+    </li>
+  </ul>
+);
+
+export default MobileNavDropdown;
