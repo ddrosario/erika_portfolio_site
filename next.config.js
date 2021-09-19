0ff294fea0ca7d2bf8d7bb3c586cca0c1b3f17fd
ddrosario <drewdelrosario@gmail.com>
2021-09-19 11:48:11 -0700

    🔧 Change to next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-const path = require('path')
+import path from 'path';
 
 module.exports = {
-  // reactStrictMode: true,
+  reactStrictMode: false,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
