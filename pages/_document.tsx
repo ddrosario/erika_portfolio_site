aaf2e97ae2135681beefc8b6413c477416f2047f
ddrosario <drewdelrosario@gmail.com>
2021-09-19 21:11:03 -0700

    🍱 Add extra font weight for Nunito font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -28,7 +28,7 @@ class MyDocument extends Document {
           {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin /> */}
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap" rel="stylesheet" />
-          <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;400&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;400;500&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
