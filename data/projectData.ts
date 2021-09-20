7277ca0b7cf1848fb816c1fa4b58bf6f7a552bad
ddrosario <drewdelrosario@gmail.com>
2021-09-19 21:04:35 -0700

    🤡 Adds in mock json for case studies

---
+++ data/projectData.ts
@@ -0,0 +1,189 @@
+export type ProjectType = 'aura' | 'nintendo' | 'carnival';
+
+export const title = {
+  aura: {
+    title: 'Aura',
+    subtext: 'A Mood-Tracking App',
+  },
+  nintendo: {
+    title: 'Nintendo eShop',
+    subtext: 'The addition of a social aspect to the Nintendo eShop for the Nintendo Switch',
+  },
+  carnival: {
+    title: 'Carnival Cruiseline',
+    subtext: 'A complete responsive redesign of the Carnival Cruise website',
+  },
+};
+
+export const overview = {
+  aura: {
+    text: 'The goal of this project was to create a mood-tracking app from scratch.',
+    img: '/assets/projects/aura/overview.png',
+  },
+  nintendo: {
+    text: 'The goal of this project was to add a social aspect to the Nintendo Switch eShop.',
+    img: '/assets/projects/nintendo/overview.png',
+  },
+  carnival: {
+    text: 'The goal of this project was to redesign the Carnival Cruise website and make it more aestitically pleasing as well as functional..',
+    img: '/assets/projects/carnival/overview.png',
+  },
+};
+
+export const research = {
+  aura: {
+    text: 'The first thing I did was write-up the Project Brief and plan out all of my time for the duration of the project. Followed by some Competitor Research.',
+    imgBrief: '/assets/projects/aura/research-brief.png',
+    imgTimeline: '/assets/projects/aura/research-timeline.png',
+    imgComp: '/assets/projects/aura/research-comp.png',
+  },
+  nintendo: {
+    text: 'I began by writing up the Project Brief and planning out all of my time for the duration of the project. Followed by some Competitor Research.',
+    imgBrief: '/assets/projects/nintendo/research-brief.png',
+    imgTimeline: '/assets/projects/nintendo/research-timeline.png',
+    imgComp: '/assets/projects/nintendo/research-comp.png',
+  },
+  carnival: {
+    text: 'The first thing I did was write-up the Project Brief and plan out all of my time for the duration of the project. ',
+    imgBrief: '/assets/projects/carnival/research-brief.png',
+    imgTimeline: '/assets/projects/carnival/research-timeline.png',
+    imgComp: '/assets/projects/carnival/research-comp.png',
+  },
+};
+
+export const interview = {
+  aura: {
+    text: 'Next, was setting up the questions and performing interviews. Then, writing up a Debrief and creating an Affinity Map to better organize the collected data.',
+    imgDebrief: '/assets/projects/aura/interview-debrief.png',
+    imgMap: '/assets/projects/aura/interview-affinity-map.png',
+    imgInterview: '/assets/projects/aura/interview-questions.png',
+  },
+  nintendo: {
+    text: 'Next, I performed interviews and wrote the Debrief followed by an Affinity Map to organize the collected data. In order to fix the issue of the tight time constraint, I added the questions to a Google Form and sent the survey out for users to answer when they had time. ',
+    imgDebrief: '/assets/projects/nintendo/interview-debrief.png',
+    imgMap: '/assets/projects/nintendo/interview-affinity-map.png',
+    imgInterview: '/assets/projects/nintendo/interview-questions.png',
+  },
+  carnival: {
+    text: 'Next, I wrote the questions and performed interviews. Then, wrote a Debrief and created an Affinity Map to better organize the collected data. One of the issues that I ran into was the time constraint, so to fix that issue, I added the questions to a Google Form and sent the survey out for users to answer when they had time. ',
+    imgDebrief: '/assets/projects/carnival/interview-debrief.png',
+    imgMap: '/assets/projects/carnival/interview-affinity-map.png',
+    imgInterview: '/assets/projects/carnival/interview-questions.png',
+  },
+};
+
+export const roadmap = {
+  aura: {
+    text: 'The next thing was to create a Feature Roadmap to plan out what I was going to prioritize. This is where I ran into the issue of the time crunch I was on and had to revise my plan!',
+    imgRoadmap: '/assets/projects/aura/roadmap.png',
+    imgRevised: '/assets/projects/aura/roadmap-revised.png',
+  },
+  nintendo: {
+    text: 'Next, going off of the information that I got from the interviews, I created a Feature Roadmap.',
+    imgRoadmap: '/assets/projects/nintendo/roadmap.png',
+    imgRevised: '/assets/projects/nintendo/roadmap-revised.png',
+  },
+  carnival: {
+    text: 'The next thing that I did was, going off of the information that I got from the interviews, create a Feature Roadmap to plan out what I was going to prioritize.',
+    imgRoadmap: '/assets/projects/carnival/roadmap.png',
+    imgRevised: '/assets/projects/carnival/roadmap-revised.png',
+  },
+};
+
+export const persona = {
+  aura: {
+    text: 'Next, I began thinking about what kind of people would be using this app and who my main audience would be, then created my persona! I also put together the Company/User Goals page to help me narrow down my goal for the app.',
+    imgPersona1: '/assets/projects/aura/persona1.png',
+    imgPersona2: '/assets/projects/aura/persona2.png',
+    imgPersona3: '/assets/projects/aura/persona3.png',
+  },
+  nintendo: {
+    text: 'Next, I created my two personas! I also put together the Company/User Goals page to help me narrow down my goal for the addition.',
+    imgPersona1: '/assets/projects/nintendo/persona1.png',
+    imgPersona2: '/assets/projects/nintendo/persona2.png',
+    imgPersona3: '/assets/projects/nintendo/persona3.png',
+  },
+  carnival: {
+    text: 'Next, I began thinking about what kind of people would be using this app and who my main audience would be, then created my two personas! I also put together the Company/User Goals page to help me narrow down my goal for the addition.',
+    imgPersona1: '/assets/projects/carnival/persona1.png',
+    imgPersona2: '/assets/projects/carnival/persona2.png',
+    imgPersona3: '/assets/projects/carnival/persona3.png',
+  },
+};
+
+export const moodBoard = {
+  aura: {
+    text: 'Next, I put together a Mood Board to get a better feel of the aesthetic of the app.',
+    img: '/assets/projects/aura/moodboard.png',
+  },
+  carnival: {
+    text: 'Next, I put together a Mood Board to get a better feel of the aesthetic of the site and match it to the original.',
+    img: '/assets/projects/carnival/moodboard.png',
+  },
+};
+
+export const planning = {
+  aura: {
+    text: 'Next, I began thinking about what kind of people would be using this app and who my main audience would be, then created my persona! I also put together the Company/User Goals page to help me narrow down my goal for the app.',
+    imgSitemap: '/assets/projects/aura/planning-sitemap.png',
+    imgUserFlow: '/assets/projects/aura/planning-userflow.png',
+    imgTaskFlow: '/assets/projects/aura/planning-taskflow.png',
+  },
+  nintendo: {
+    text: 'Next, I created my two personas! I also put together the Company/User Goals page to help me narrow down my goal for the addition.',
+    imgSitemap: '/assets/projects/nintendo/planning-sitemap.png',
+    imgUserFlow: '/assets/projects/nintendo/planning-userflow.png',
+    imgTaskFlow: '/assets/projects/nintendo/planning-taskflow.png',
+  },
+  carnival: {
+    text: 'Next, I began thinking about what kind of people would be using this app and who my main audience would be, then created my two personas! I also put together the Company/User Goals page to help me narrow down my goal for the addition.',
+    imgSitemap: '/assets/projects/carnival/planning-sitemap.png',
+    imgUserFlow: '/assets/projects/carnival/planning-userflow.png',
+    imgTaskFlow: '/assets/projects/carnival/planning-taskflow.png',
+  },
+};
+
+export const wireframes = {
+  aura: {
+    text: 'I then put the pages together as wireframes using Sketch and went ahead with a prototype testing using Maze as the platform.',
+    img: '/assets/projects/aura/wireframes.png',
+  },
+  nintendo: {
+    text: 'Then, I sketched out my plans for all of the pages of the app!',
+    img: '/assets/projects/nintendo/wireframes.png',
+  },
+  carnival: {
+    text: 'Then, I sketched out my plans for all of the pages of the app!',
+    img: '/assets/projects/carnival/wireframes.png',
+  },
+};
+
+export const uiKit = {
+  aura: {
+    text: 'The next thing I focused on was creating a UI Kit to get the aesthetic for the app!',
+    img: '/assets/projects/aura/ui-kit.png',
+  },
+  nintendo: {
+    text: 'The next thing I focused on was creating a UI Kit to get the aesthetic for the eShop the same as the original!',
+    img: '/assets/projects/nintendo/ui-kit.png',
+  },
+  carnival: {
+    text: 'The next thing I focused on was creating a UI Kit to get the aesthetic for the eShop the same as the original!',
+    img: '/assets/projects/carnival/ui-kit.png',
+  },
+};
+
+export const finalProduct = {
+  aura: {
+    text: 'Finally, I added the branding to the app! I ran into one more issue of the ‘year at a glance’ idea that I had had and turned it into a choosing from past months button page instead in the final version as well as added a store for users to purchase new items for their journaling pages!',
+    img: '/assets/projects/aura/final.png',
+  },
+  nintendo: {
+    text: 'Finally, I added the branding to the eShop and its changes!',
+    img: '/assets/projects/nintendo/final.png',
+  },
+  carnival: {
+    text: 'Finally, I added the branding to the eShop and its changes!',
+    img: '/assets/projects/carnival/final.png',
+  },
+};
