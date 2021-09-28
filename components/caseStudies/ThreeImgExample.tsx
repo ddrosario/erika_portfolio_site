4d6482b8b597501988ed3b5cf7e15708e2b7feca
ddrosario <drewdelrosario@gmail.com>
2021-09-27 21:20:14 -0700

    🎨 Create reusable 1, 2, and 3 image components for case studies

---
+++ components/caseStudies/ThreeImgExample.tsx
@@ -0,0 +1,123 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react';
+
+import BorderedCard from '@components/hoc/BorderedCard';
+
+import styles from '@styles/projects.module.scss';
+import useLightbox from '@util/hooks/useLightbox';
+
+interface ThreeImgExampleProps {
+  header: string;
+  text: string;
+  img1: string;
+  img2: string;
+  img3: string;
+  label1?: string | null;
+  label2?: string | null;
+  label3?: string | null;
+  fullImg1: string;
+  fullImg2: string;
+  fullImg3: string;
+  alt1?: string;
+  alt2?: string;
+  alt3?: string;
+}
+
+export default function ThreeImgExample({
+  header,
+  text,
+  img1,
+  img2,
+  img3,
+  label1 = null,
+  label2 = null,
+  label3 = null,
+  fullImg1,
+  fullImg2,
+  fullImg3,
+  alt1 = '',
+  alt2 = '',
+  alt3 = '',
+}: ThreeImgExampleProps) {
+  const [Lightbox, openLightbox] = useLightbox();
+  return (
+    <>
+      <BorderedCard>
+        <section className={styles.caseStudy}>
+          <h2 className={styles.h2}>{header}</h2>
+          <p className={styles.projectText}>{text}</p>
+          <div className={styles.threeImgContainer}>
+            <div className={styles.imgBorder}>
+              <button
+                className={styles.img}
+                onClick={() => {
+                  openLightbox(fullImg1);
+                }}
+                type="button"
+              >
+                <img
+                  src={img1}
+                  alt={alt1}
+                />
+              </button>
+              {label1 && (
+              <p className={styles.imgLabel}>
+                {label1}
+              </p>
+              )}
+            </div>
+            <div className={styles.imgBorder}>
+              <button
+                className={styles.img}
+                onClick={() => {
+                  openLightbox(fullImg2);
+                }}
+                type="button"
+              >
+                <img
+                  src={img2}
+                  alt={alt2}
+                />
+              </button>
+              {label2 && (
+              <p className={styles.imgLabel}>
+                {label2}
+              </p>
+              )}
+            </div>
+            <div className={styles.breakFlex} />
+            <div className={styles.thirdImg}>
+              <button
+                className={styles.img}
+                onClick={() => {
+                  openLightbox(fullImg3);
+                }}
+                type="button"
+              >
+                <img
+                  src={img3}
+                  alt={alt3}
+                />
+              </button>
+              {label3 && (
+              <p className={styles.imgLabel}>
+                {label3}
+              </p>
+              )}
+            </div>
+          </div>
+        </section>
+      </BorderedCard>
+      {Lightbox}
+    </>
+  );
+}
+
+ThreeImgExample.defaultProps = {
+  label1: null,
+  label2: null,
+  label3: null,
+  alt1: '',
+  alt2: '',
+  alt3: '',
+};
