ed837485dfb1020ec3dc8b9200f5ce46cf49cc47
ddrosario <drewdelrosario@gmail.com>
2021-11-09 07:32:58 -0800

    🐛 Fix failing load of header

--- components/hoc/RenderDefault.tsx
+++
@@ -1,8 +0,0 @@
-import { useMediaQuery } from 'react-responsive';
-
-const RenderDefault = ({ children }: any) => {
-  const isNotMobile = useMediaQuery({ minWidth: 768 });
-  return isNotMobile ? children : null;
-};
-
-export default RenderDefault;
