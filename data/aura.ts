135cdef7968d3fd555efaa15c5ca5d67c54e0378
ddrosario <drewdelrosario@gmail.com>
2021-11-20 20:38:49 -0800

    🤡 Mock data for new case studies

---
+++ data/aura.ts
@@ -0,0 +1,181 @@
+export const empathize = [
+  {
+    title: 'Competitor Research',
+    body: 'I did some research into similar apps to see how they worked and what they featured for the user to interact with and use. It was found that many apps do not allow for note-taking or the ability to track moods multiple times a day. This then became a question of whether or not these two features were something users really wanted in a mood-tracking app.',
+    images: [
+      {
+        webp: '',
+        jpg: '',
+        jpgLg: '',
+      },
+    ],
+  },
+  {
+    title: 'Interview Questions',
+    body: 'Now that I knew what my focus was going to be, I came up with questions and conducted interviews online, taking notes on the responses that I got. If the participant wasn’t available, or our schedules clashed, I sent out a survey for them to fill out instead when they had time.',
+    images: [
+      {
+        webp: '',
+        jpg: '',
+        jpgLg: '',
+      },
+    ],
+  },
+  {
+    title: 'Interview Debrief',
+    body: 'After the interviews and surveys were completed, I wrote a debrief to organize the data to present to my colleagues. The results showed that many people who use some form of mood-tracking enjoy having a journaling option, even if just for a quick blurp of why they felt the way they did at the time of tracking. Many also had suggestions of adding audio clips such as rain sounds and yoga poses to the app, which was an interesting idea to explore. The major takeaway from this debrief was that the average user of mood-tracking apps would like a journaling option from their app.',
+    images: [
+      {
+        webp: '',
+        jpg: '',
+        jpgLg: '',
+      },
+    ],
+  },
+  {
+    title: 'Persona',
+    body: 'I took all of the information found in the debrief and created a persona based on the information I received from the interviews, focusing on who exactly my target audience would be. Since most of the interviewees were female, I went with a female persona with an age that matched my potential users.',
+    images: [
+      {
+        webp: '',
+        jpg: '',
+        jpgLg: '',
+      },
+    ],
+  },
+];
+
+export const define = [
+  {
+    title: 'Roadmap',
+    body: 'I prioritized which features I would be focusing on first and what could come later before running it past my colleagues for feedback since I knew the time crunch would be an issue.',
+    images: [
+      {
+        webp: '',
+        jpg: '',
+        jpgLg: '',
+      },
+    ],
+  },
+  {
+    title: 'Roadmap Iterations',
+    body: 'Due to the time crunch, I had to move some things around since it wasn’t possible to get everything completed by the deadline. I placed my focus on the journaling feature since that was the most requested during interviews and moved the rest of the features to be created at a later time.',
+    images: [
+      {
+        webp: '',
+        jpg: '',
+        jpgLg: '',
+      },
+    ],
+  },
+  {
+    title: 'Mood Board',
+    body: 'Once I had the priority features down, I then created a mood board to get a feel of the aesthetic of the app that I was going for based on what the expectations of the users were. When asked, users defined the term ‘relaxing’ in their own words, most citing water, beaches, and the color blue as a place of calm. ',
+    images: [
+      {
+        webp: '',
+        jpg: '',
+        jpgLg: '',
+      },
+    ],
+  },
+];
+
+export const ideate = [
+  {
+    title: 'Sitemap',
+    body: 'Next, it was time to focus on the eventual layout of the app, starting with the first thing the user sees when the app is opened and what the other pages were that would be offered.',
+    images: [
+      {
+        webp: '',
+        jpg: '',
+        jpgLg: '',
+      },
+    ],
+  },
+  {
+    title: 'Userflow',
+    body: 'With my persona in mind, I defined how the user would move through the app, which pages they would interact with on a normal day, and what they would do on those pages.',
+    images: [
+      {
+        webp: '',
+        jpg: '',
+        jpgLg: '',
+      },
+    ],
+  },
+  {
+    title: 'UI Kit',
+    body: 'After a lot of research and planning, I then came up with the official aesthetic of the app, choosing the cool, calm blue colors and a furry mascot to add to the relaxed feel of the app.',
+    images: [
+      {
+        webp: '',
+        jpg: '',
+        jpgLg: '',
+      },
+    ],
+  },
+  {
+    title: 'Sketches',
+    body: 'I then sketched out all of the pages and features that the users would be interacting with before presenting them to my colleagues for feedback. After feedback, a few changes were made to simplify the homepage to make it easier for users to navigate the app.',
+    images: [
+      {
+        webp: '',
+        jpg: '',
+        jpgLg: '',
+      },
+      {
+        webp: '',
+        jpg: '',
+        jpgLg: '',
+      },
+      {
+        webp: '',
+        jpg: '',
+        jpgLg: '',
+      },
+    ],
+  },
+];
+
+export const prototyping = [
+  {
+    title: 'Wireframes',
+    body: 'After the feedback from the team was implemented,  then created digitized versions of my sketches and added some of the features to them such as the menus and basic navigation before, once again, presenting the work thus far for feedback.',
+    images: [
+      {
+        webp: '',
+        jpg: '',
+        jpgLg: '',
+      },
+    ],
+  },
+];
+
+export const testing = [
+  {
+    title: 'Usability Testing',
+    body: 'Using Maze as a platform, I conducted a click-test and a navigation test to make sure the designs were accessible and navigable, and to understand better how users would move through the app and what could still be improved before presenting my findings.',
+    images: [
+      {
+        webp: '',
+        jpg: '',
+        jpgLg: '',
+      },
+    ],
+  },
+];
+
+export const implement = [
+  {
+    title: 'Final Product',
+    body: 'Finally, I added the final iterations of the product and the branding to the app for a relaxing, minimal design that would be easy for users to navigate and interact with. The addition of a store, washi tape, fonts, and images added to the overall journaling aspect of the app.',
+    images: [
+      {
+        webp: '',
+        jpg: '',
+        jpgLg: '',
+      },
+    ],
+  },
+];
---
+++ data/hov.ts
@@ -0,0 +1,77 @@
+export const empathize = [
+  {
+    title: 'Internal Interviews',
+    body: 'I conducted several internal interviews to better understand how other team members viewed pages of the site and what could be done better.',
+    images: [
+      {
+        webp: '',
+        jpg: '',
+        jpgLg: '',
+      },
+    ],
+  },
+];
+
+export const define = [
+  {
+    title: 'Mood Board',
+    body: 'I created a mood board for the pages that the project manager wanted me to create wireframes and branding for so the team could later vote on them and decide what style we would design for.',
+    images: [
+      {
+        webp: '',
+        jpg: '',
+        jpgLg: '',
+      },
+    ],
+  },
+];
+
+export const ideate = [
+  {
+    title: 'Userflow',
+    body: 'I was tasked with creating a User flow to visualize how the user would move through the website. For this, I used Whimsical and allowed anyone with a link to access it and make comments.',
+    images: [
+      {
+        webp: '',
+        jpg: '',
+        jpgLg: '',
+      },
+    ],
+  },
+  {
+    title: 'UI Kit',
+    body: 'I created a UI Kit to help the team visualize what themes I was going for and what elements I used on the pages I’ve created so far.',
+    images: [
+      {
+        webp: '',
+        jpg: '',
+        jpgLg: '',
+      },
+    ],
+  },
+];
+
+export const prototyping = [
+  {
+    title: 'Wireframes',
+    body: 'I digitized my ideas using Sketch and plotted out all of the pages I would be creating thus far.',
+    images: [
+      {
+        webp: '',
+        jpg: '',
+        jpgLg: '',
+      },
+    ],
+  },
+  {
+    title: 'Branding',
+    body: 'I was asked to add my branding to the homepage wireframe and then present it to the team. We later decided on dark mode for the entire site.',
+    images: [
+      {
+        webp: '',
+        jpg: '',
+        jpgLg: '',
+      },
+    ],
+  },
+];
