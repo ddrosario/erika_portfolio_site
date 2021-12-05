1ecd2beaccbcba80b2a613d25cc287a3ce2c5120
ddrosario <drewdelrosario@gmail.com>
2021-12-05 00:56:41 -0800

    🚧 Create base for ViewProject CTA component

---
+++ components/ViewProject.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Link from 'next/link';
+
+import styles from '@styles/ViewProject.module.scss';
+
+interface ViewProjectProps {
+  href: string;
+  ctaText: string;
+  header: string;
+  body: string;
+}
+
+const ViewProject = ({
+  href,
+  ctaText,
+  header,
+  body,
+}: ViewProjectProps) => (
+  <div className={styles.root}>
+    <h3 className={styles.h3}>{header}</h3>
+    <p className={styles.body}>{body}</p>
+    <div className={styles.linkContainer}>
+      <Link
+        href={href}
+        passHref
+      >
+        <a
+          className={styles.link}
+          href="replace"
+        >
+          {ctaText}
+        </a>
+      </Link>
+    </div>
+  </div>
+);
+
+export default ViewProject;
