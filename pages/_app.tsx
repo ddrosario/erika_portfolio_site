29e96d6585f24657ce1138e964d99a512c2ea659
ddrosario <drewdelrosario@gmail.com>
2021-12-05 00:52:44 -0800

    ➕ Add smooth scroll polyfill for Safari

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import '@styles/globals.scss';
 import type { AppProps } from 'next/app';
+import smoothscroll from 'smoothscroll-polyfill';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
+    smoothscroll.polyfill();
+  }, []);
   // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />;
 }
