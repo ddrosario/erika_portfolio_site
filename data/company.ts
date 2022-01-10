7ef49fc329a7f7f810355967d6e58b80ea75b2cb
ddrosario <drewdelrosario@gmail.com>
2022-01-09 16:43:22 -0800

    🤡 Add data for company blurb

---
+++ data/company.ts
@@ -0,0 +1,16 @@
+export enum PortfolioCompanies {
+  docusign = 'docusign',
+}
+
+export const companies: {
+  [companyKey: string]: {
+    title: string;
+    blurb: string;
+  };
+} = {
+  docusign: {
+    title: 'DocuSign',
+    blurb:
+      "I have had to sign many documents over the years, from my car's lease to my apartment's lease, and DocuSign has always made it easy to sign agreements online. Making users' lives easier and more efficient is what I'm all about, and eliminating the need to drive to an office to sign documents and simply doing it over email is so much more convenient. This is why I have chosen to apply to DocuSign!",
+  },
+};
